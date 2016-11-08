fd7556b473dc6289c09758f47cd11cc743da8f9f
hbeckmann <hunterbeckmann@gmail.com>
2016-11-08 00:23:52 -0700

    Reverse String

---
+++ Practice/ReverseString.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace ReverseString
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            string example = "Hello";
+            char[] exampleArr = example.ToCharArray();
+            string reverse = "";
+
+
+            for(int x = exampleArr.Length - 1; x >= 0; x--)
+            {
+                reverse += exampleArr[x]; 
+            }
+
+            Console.WriteLine(reverse);
+            Console.ReadLine();
+
+
+        }
+    }
+}
