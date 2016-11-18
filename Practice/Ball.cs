dd66a6abeecfc9f75013748bc8687c98aed1ab55
hbeckmann <hunterbeckmann@gmail.com>
2016-11-17 20:37:32 -0700

    Ball and Color practice

---
+++ Practice/Ball.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColorBall
+{
+    class Ball
+    {
+
+        private int size;
+        private float radius;
+        private Color color;
+        private int timesThrown;
+
+        public Ball(int size, float radius, Color color, int timesThrown)
+        {
+            this.size = size;
+            this.radius = radius;
+            this.color = color;
+            this.timesThrown = timesThrown;
+        }
+
+        public void Pop()
+        {
+            this.size = 0;
+        }
+
+        public void Throw()
+        {
+            if(this.size != 0)
+            {
+                this.timesThrown++;
+            }
+
+        }
+
+        public int GetTimesThrown()
+        {
+            return timesThrown;
+        }
+
+    }
+}
---
+++ Practice/Color.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColorBall
+{
+    class Color
+    {
+        private int red = 0;
+        private int blue = 0;
+        private int green = 0;
+        private int alpha = 0;
+
+        public Color(int red, int blue, int green, int alpha)
+        {
+            this.red = red;
+            this.blue = blue;
+            this.green = green;
+            this.alpha = alpha;
+        }
+
+        public Color(int red, int green, int blue)
+        {
+            this.red = red;
+            this.blue = blue;
+            this.green = green;
+            this.alpha = 250;
+        }
+
+        public void GetColors()
+        {
+            Console.WriteLine($"Red: {this.red}");
+            Console.WriteLine($"Green: {this.green}");
+            Console.WriteLine($"Blue: {this.blue}");
+        }
+
+        public int GetRed()
+        {
+            return red;
+        }
+
+        public void SetRed(int red)
+        {
+            this.red = red;
+        }
+
+        public int GetBlue()
+        {
+            return blue;
+        }
+
+        public void SetBlue(int blue)
+        {
+            this.blue = blue;
+        }
+
+        public int GetGreen()
+        {
+            return green;
+        }
+
+        public void SetGreen(int green)
+        {
+            this.green = green;
+        }
+
+        public int GetAlpha()
+        {
+            return alpha;
+        }
+
+        public void SetAlpha(int alpha)
+        {
+            this.alpha = alpha;
+        }
+
+        public int GetGrayscale()
+        {
+            return (red + blue + green) / 3;
+        }
+    }
+}
