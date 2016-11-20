ca61356eea0f39a8a78023b6d51243245e0b4494
hbeckmann <hunterbeckmann@gmail.com>
2016-11-19 20:31:04 -0700

    Palindrome example

---
+++ Practice/Palindrome.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(checkForPalindrome("racecar"));
+            Console.WriteLine(checkForPalindrome("hunter"));
+            Console.WriteLine(checkForPalindrome("radar"));
+            Console.ReadKey();
+
+        }
+
+        public static bool checkForPalindrome(string word)
+        {
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word[i] != word[word.Length - i - 1])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
